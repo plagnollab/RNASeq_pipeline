f47d2203653568781218f37035b1181e3f43159f
Mr Jack Humphrey <jack.humphrey@ucl.ac.uk>
2016-06-02 15:43:15 +0100

    multiple fastqs and separate fastQC step

--- example/submission_example_v8_star.sh
+++ example/submission_example_v8_star.sh
@@ -11,6 +11,9 @@ species=Human_hg38
 submit=yes
 force=yes
 
+trim_galore=yes
+
+step0_QC=yes
 starStep1a=yes
 starStep1b=yes
 starStep2=yes
@@ -29,7 +32,7 @@ dataframe=support/Owen_blood.tab
 code=Owen_blood
 species=human_hg38
 
-sh $pipeline --iFolder ${iFolder} --oFolder ${oFolder} --dataframe ${dataframe} --code ${code} --prepareCounts ${prepareCounts} --Rdexseq ${Rdexseq} --Rdeseq ${Rdeseq}  --summary ${summary} --starStep1a ${starStep1a} --starStep1b ${starStep1b} --starStep2 ${starStep2} --species ${species} --submit ${submit} --force ${force}
+sh $pipeline --iFolder ${iFolder} --oFolder ${oFolder} --dataframe ${dataframe} --code ${code} --prepareCounts ${prepareCounts} --Rdexseq ${Rdexseq} --Rdeseq ${Rdeseq}  --summary ${summary} --step0_QC ${step0_QC} --trim_galore ${trim_galore} --starStep1a ${starStep1a} --starStep1b ${starStep1b} --starStep2 ${starStep2} --species ${species} --submit ${submit} --force ${force}
 
 
 
